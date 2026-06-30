cbc71e26051968d5e670cd298cf29a9767944fb6
Ishant-hub <gargishant766@gmail.com>
2026-06-30 20:52:46 +0530

    Update Eternal Clone

--- Enternal/js1.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function () {
-  
-  const loader = document.getElementById("loading");
-
-  setTimeout(() => {
-    loader.classList.add("fade-out");
-  }, 100);
-});
